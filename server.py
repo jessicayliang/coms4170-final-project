53757b8102d98f6a54081757f6a737f58fb77b19
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2024-04-14 20:53:21 -0400

    mock data for wines

--- server.py
+++ server.py
@@ -8,7 +8,23 @@ from flask import render_template
 from flask import Response, request, jsonify
 from markupsafe import Markup
 
+
+def load_wines():
+    """
+    Load wines from the JSON file.
+    :return: a python dictionary mapping wine name to image, pairing list, description, and next wine.
+
+    """
+    with open('wines.json') as file:
+        data = json.load(file)
+    return data
+
+
+# The variable wines holds the wine dict
+wines = load_wines()
+
 app = Flask(__name__)
+
 
 # ROUTES
 @app.route('/')
@@ -17,8 +33,4 @@ def home():
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
-
-
-
-
+    app.run(debug=True)
